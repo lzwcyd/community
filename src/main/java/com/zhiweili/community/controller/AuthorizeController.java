eb825268acc50e7f9bcd1aa0cfa238c020988780
lzwcyd <1663905985@qq.com>
2019-10-15 20:46:45 +0800

    修改端口号

--- src/main/java/com/zhiweili/community/controller/AuthorizeController.java
+++ src/main/java/com/zhiweili/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.zhiweili.community.dto.AccessTokenDto;
 import com.zhiweili.community.dto.GiteeUserDto;
 import com.zhiweili.community.provider.GiteeProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,15 +15,23 @@ public class AuthorizeController {
     @Autowired
     private GiteeProvider giteeProvider;
 
+    @Value("${gitee.client.id}")
+    private String clientId;
+
+    @Value("${gitee.client.secret}")
+    private String clientSecret;
+
+    @Value("${gitee.Redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code){
-        System.out.println(code);
         AccessTokenDto accessTokenDto = new AccessTokenDto();
-        accessTokenDto.setClient_id("fde18c7491c1c81c90a3cb46022ad307dceee1b1bdc68d66fb64acaeab613034");
-        accessTokenDto.setClient_secret("db0e5b63807d8a8f8de5e9e1017e27c5509cded8725c875998f3745f1e57fa88");
+        accessTokenDto.setClient_id(clientId);
+        accessTokenDto.setClient_secret(clientSecret);
         accessTokenDto.setCode(code);
         accessTokenDto.setGrant_type("authorization_code");
-        accessTokenDto.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDto.setRedirect_uri(redirectUri);
         String accessToken = giteeProvider.getAccessToken(accessTokenDto);
         GiteeUserDto user = giteeProvider.getUser(accessToken);
         return "index";
